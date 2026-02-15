5b1d1a078a9113ef5fb9badc80e39ebac508809a
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-02-15 20:01:43 +0100

    moved components from datarepo

---
+++ oarepo_rdm/ui/components/__init__.py
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2026 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-rdm (see https://github.com/oarepo/oarepo-rdm).
+#
+# oarepo-rdm is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""oarepo-rdm UI components."""
+
+from __future__ import annotations
+
+from .communities_memberships_dump import CommunitiesMembershipsComponent
+from .rdm_vocabularies_dump import RDMVocabularyOptionsComponent
+
+__all__ = [
+    "CommunitiesMembershipsComponent",
+    "RDMVocabularyOptionsComponent",
+]
