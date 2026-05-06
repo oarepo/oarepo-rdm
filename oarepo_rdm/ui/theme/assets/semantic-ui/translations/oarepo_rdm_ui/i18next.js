092462e6e227ef1d9fd8eb0685e9fdacda17bead
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-05-06 11:57:10 +0200

    translations

--- oarepo_rdm/ui/theme/assets/semantic-ui/translations/oarepo_rdm_ui/i18next.js
+++ oarepo_rdm/ui/theme/assets/semantic-ui/translations/oarepo_rdm_ui/i18next.js
@@ -1,8 +1,7 @@
-//  Copyright (c) 2025 CESNET z.s.p.o.
-
-//  This file is a part of oarepo-rdm (see https://github.com/oarepo/oarepo-rdm).
-
-//  oarepo-rdm is free software; you can redistribute it and/or modify it
+// This file is part of React-Invenio-Deposit
+// Copyright (C) 2021 Graz University of Technology.
+//
+// Invenio-app-rdm is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
 import i18n from "i18next";
