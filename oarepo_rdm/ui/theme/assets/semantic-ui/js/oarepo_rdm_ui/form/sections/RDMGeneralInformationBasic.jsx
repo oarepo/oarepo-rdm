49b93470567d671f5d6360a5f55c7aaed258659d
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-06-29 10:16:42 +0200

    fix: 18next import

--- oarepo_rdm/ui/theme/assets/semantic-ui/js/oarepo_rdm_ui/form/sections/RDMGeneralInformationBasic.jsx
+++ oarepo_rdm/ui/theme/assets/semantic-ui/js/oarepo_rdm_ui/form/sections/RDMGeneralInformationBasic.jsx
@@ -10,7 +10,7 @@
 import React from "react";
 import { buildUID } from "react-searchkit";
 import Overridable from "react-overridable";
-import i18next from "i18next";
+import { i18next } from "@translations/oarepo_rdm";
 import { EDTFSingleDatePicker, CreatibutorsField } from "@js/oarepo_ui/forms";
 import { TitlesField, ResourceTypeField } from "@js/invenio_rdm_records";
 import { PIDFieldList } from "../components";
